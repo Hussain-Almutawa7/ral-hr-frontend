dc5bc025ead3c2baf38f70898ba30872c90a736a
Hussain-Almutawa7 <hussain.almutawa99@gmail.com>
2026-08-31 15:11:27 +0300

    Load my attendance data

--- src/pages/attendance/MyAttendance.jsx
+++ src/pages/attendance/MyAttendance.jsx
@@ -1,5 +1,51 @@
+import { useState, useEffect } from "react";
+import { getMyAttendance } from "../../services/attendanceService";
+import { Link } from "react-router";
+
 const MyAttendance = () => {
-    return <h1>My Attendance</h1>;
+    const [attendances, setAttendances] = useState([]);
+    const [error, setError] = useState("");
+    const [isLoading, setIsLoading] = useState(true);
+
+    const loadAttendance = async () => {
+        try {
+            const attendanceData = await getMyAttendance();
+            setAttendances(attendanceData)
+        } catch (e) {
+            setError(e.message);
+        } finally {
+            setIsLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        loadAttendance();
+    }, []);
+
+    const formatDate = (date) => {
+        return new Date(date).toLocaleDateString("en-GB", {
+            time: "Asia/Bahrain"
+        });
+    }
+
+    const formatTime = (time) => {
+        if(!time) return "-";
+
+        return new Date(time).toLocaleTimeString("en-GB", {
+            hour: "2-digit",
+            minute: "2-digit",
+            timeZone: "Asia/Bahrain"
+        });
+    }
+
+    if(isLoading) return <p>Loading attendance....</p>
+
+    return(
+        <>
+        <h1>My Attendance</h1>
+        <Link to="/attendance/check-in">Check In / Out</Link>
+        </>
+    );
 };
 
 export default MyAttendance;
