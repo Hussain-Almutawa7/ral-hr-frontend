dde115c50f25ea86f536f23f620f243331bd411c
Hussain-Almutawa7 <hussain.almutawa99@gmail.com>
2026-08-31 10:41:37 +0300

    Add get checkins function

--- src/services/attendanceService.js
+++ src/services/attendanceService.js
@@ -25,6 +25,25 @@ const createCheckin = async (checkinData) => {
     }
 }
 
+const getMyCheckins = async () => {
+    try {
+        const res = await fetch(`${CHECK_IN_URL}/me`, {
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem("token")}`
+            },
+        });
+
+        const data = await res.json();
+
+        if (!res.ok) throw new Error(data.err);
+
+        return data;
+    } catch (e) {
+        throw Error(e.message);
+    }
+}
+
 export {
     createCheckin,
+    getMyCheckins,
 }
