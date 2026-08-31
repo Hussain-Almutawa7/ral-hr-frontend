9b078ae279162acd2814e48af54b2ff4d79a2574
Hussain-Almutawa7 <hussain.almutawa99@gmail.com>
2026-08-31 11:41:56 +0300

    Add correct attendance function

--- src/services/attendanceService.js
+++ src/services/attendanceService.js
@@ -181,6 +181,26 @@ const createCorrection = async (correctionData) => {
     }
 }
 
+const correctAttendance = async (correctionId) => {
+    try {
+        const res = await fetch(`${CORRECTION_URL}/${correctionId}/correct`, {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${localStorage.getItem("token")}`
+            },
+        });
+
+        const data = await res.json();
+
+        if (!res.ok) throw new Error(data.err);
+
+        return data;
+    } catch (e) {
+        throw Error(e.message);
+    }
+}
+
 export {
     createCheckin,
     getMyCheckins,
@@ -191,4 +211,5 @@ export {
     updateOvertime,
     getCorrections,
     createCorrection,
+    correctAttendance,
 }
