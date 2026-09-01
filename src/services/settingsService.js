dd4ad28fc1cbcdd67a16e1b6187faa3fdf45111d
Hussain-Almutawa7 <hussain.almutawa99@gmail.com>
2026-09-01 11:17:07 +0300

    Add company service functions

--- src/services/settingsService.js
+++ src/services/settingsService.js
@@ -2,5 +2,44 @@ const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/api`;
 
 // Company
 const getCompany = async () => {
-    
+    try {
+        const res = await fetch(`${BASE_URL}/company`, {
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem("token")}`
+            },
+        });
+
+        const data = await res.json();
+
+        if (!res.ok) throw new Error(data.err);
+
+        return data;
+    } catch (e) {
+        throw Error(e.mesage);
+    }
+}
+
+const updateCompany = async (companyData) => {
+    try {
+        const res = await fetch(`${BASE_URL}/company`, {
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${localStorage.getItem("token")}`
+            },
+            body: JSON.stringify(companyData)
+        });
+
+        const data = await res.json();
+
+        if (!res.ok) throw new Error(data.err);
+
+        return data;
+    } catch (e) {
+        throw Error(e.message);
+    }
+}
+
+export {
+    getCompany,
+    updateCompany,
 }
