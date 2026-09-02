915a48631ea32ae5a7b32cd24221e3d453ddcc8f
Hussain-Almutawa7 <hussain.almutawa99@gmail.com>
2026-09-02 13:46:16 +0300

    Adjust landing page and signout navigation

--- src/components/layout/Nav.jsx
+++ src/components/layout/Nav.jsx
@@ -3,11 +3,11 @@ import { Link, useNavigate} from "react-router";
 const Nav = ({ user, setUser }) => {
     const navigate = useNavigate();
 
-    const handleSignOut = () => {
-        localStorage.removeItem("token");
-        setUser(null);
-        navigate("/sign-in");
-    }
+const handleSignOut = () => {
+    localStorage.removeItem("token");
+    setUser(null);
+    navigate("/", { replace: true });
+}
 
     return (
         <nav className="top-nav">
